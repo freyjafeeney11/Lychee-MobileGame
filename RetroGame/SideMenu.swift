cf492969364cc2aa0f7841c69f47be97a4eca913
Chase <beaucagej20@gmail.com>
2023-10-21 18:44:22 -0400

    SideMenu needs to be filled still

--- RetroGame/SideMenu.swift
+++ RetroGame/SideMenu.swift
@@ -11,37 +11,12 @@ import GameplayKit
 
 class SideMenu: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    var runnerButton: SKSpriteNode?
-    var menuBar: SKSpriteNode?
+
     
     
     override func didMove(to view: SKView) {
         
         
-        let room = SKSpriteNode(imageNamed: "FullLivingRoom")
-        let player = SKSpriteNode(imageNamed: "catbat_prototype")
-        menuBar = SKSpriteNode(imageNamed: "SideMenuClosed")
-        runnerButton = SKSpriteNode(imageNamed: "RunnerButton")
-        
-        runnerButton?.position = CGPoint(x: size.width * 0.8, y: size.height * 0.7)
-        
-        menuBar?.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
-        
-        backgroundColor = SKColor.white
-        room.setScale(0.85)
-        runnerButton?.setScale(0.21)
-        menuBar?.setScale(3)
-
-        
-        room.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
-        player.position = CGPoint(x: size.width * 0.45, y: size.height * 0.4)
-
-        addChild(room)
-        addChild(player)
-        addChild(runnerButton!)
-        addChild(menuBar!)
         
     }
     
@@ -49,18 +24,6 @@ class SideMenu: SKScene {
         for touch in touches {
             let location = touch.location(in: self)
 
-            if runnerButton?.contains(location) == true {
-                // Transition to the runner game scene
-                let runnerGame = Runner(size: size)
-                runnerGame.scaleMode = .aspectFill
-                view?.presentScene(runnerGame)
-            }
-            //if menuBar?.contains(location) == true {
-                // Opens Side Menu
-            //    let sideMenu = SideMenu(size: size)
-            //    sideMenu.scaleMode = .aspectFill
-            //    view?.presentScene(sideMenu)
-            //}
         }
     }
 }
