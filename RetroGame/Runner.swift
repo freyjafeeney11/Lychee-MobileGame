d7e8921bb090b0d2a9edd9ef5ddb821ba011dc9e
TseringLhakhang <tlhakha1@uvm.edu>
2023-11-09 18:41:39 -0500

    Parallax looks okay, working on getting character

--- RetroGame/Runner.swift
+++ RetroGame/Runner.swift
@@ -7,12 +7,14 @@
 
 import SpriteKit
 
-class Runner: SKScene{
+class Runner: SKScene, SKPhysicsContactDelegate{
     
     let cityFront = SKSpriteNode(imageNamed: "FrontBuilding")
     let cityBack = SKSpriteNode(imageNamed: "BackBuilding")
     let cityFront2 = SKSpriteNode(imageNamed: "FrontBuilding")
     let cityBack2 = SKSpriteNode(imageNamed: "BackBuilding")
+    let cityFront3 = SKSpriteNode(imageNamed: "FrontBuilding")
+    let cityBack3 = SKSpriteNode(imageNamed: "BackBuilding")
     
     
     let sky = SKSpriteNode(imageNamed: "NightClouds")
@@ -25,33 +27,47 @@ class Runner: SKScene{
     let skyVelocity: CGFloat = 1.0
     
     
+    let character = SKSpriteNode(imageNamed: "chicken-hamster")
+        
     override func didMove(to view: SKView){
         self.createCity()
+        self.addCharacter()
+        self.physicsWorld.contactDelegate = self
     }
     
     func createCity() {
         
         cityFront.position = CGPoint(x: size.width * 0.5, y: size.height * 0.35)
-        cityFront.setScale(2)
+        cityFront.setScale(2.5)
         cityFront.zPosition = 3
         self.addChild(cityFront)
         
         cityBack.position = CGPoint(x: cityFront.position.x - cityFront.size.width, y: size.height * 0.35)
-        cityBack.setScale(2)
+        cityBack.setScale(2.5)
         cityBack.zPosition = 2
         self.addChild(cityBack)
         
         
         // Having it follow the 1st city front positioning directly after
         cityFront2.position = CGPoint(x: cityFront.position.x + cityFront.size.width, y: size.height * 0.35)
-        cityFront2.setScale(2)
+        cityFront2.setScale(2.5)
         cityFront2.zPosition = 3
         self.addChild(cityFront2)
         
         cityBack2.position = CGPoint(x: cityFront2.position.x - cityFront2.size.width, y: size.height * 0.35)
-        cityBack2.setScale(2)
+        cityBack2.setScale(2.5)
         cityBack2.zPosition = 2
         self.addChild(cityBack2)
+        
+        cityFront3.position = CGPoint(x: cityBack2.position.x - cityBack2.size.width, y: size.height * 0.35)
+        cityFront3.setScale(2.5)
+        cityFront3.zPosition = 3
+        self.addChild(cityFront3)
+                
+        cityBack3.position = CGPoint(x: cityFront3.position.x - cityFront3.size.width, y: size.height * 0.35)
+        cityBack3.setScale(2.5)
+        cityBack3.zPosition = 2
+        self.addChild(cityBack3)
         
         sky.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
         sky.setScale(3)
@@ -65,7 +81,14 @@ class Runner: SKScene{
         sky3.setScale(3)
         self.addChild(sky3)
         
-        
+    }
+    func addCharacter() {
+        character.position = CGPoint(x: size.width * 0.2, y: cityFront.position.y + cityFront.size.height * 0.5 + character.size.height * 0.5)
+        character.zPosition = 3
+        character.physicsBody = SKPhysicsBody(rectangleOf: character.size)
+        character.physicsBody?.isDynamic = true
+        character.physicsBody?.categoryBitMask = 1
+        self.addChild(character)
     }
     
     override func update(_ currentTime: TimeInterval){
@@ -74,7 +97,9 @@ class Runner: SKScene{
         cityBack.position = CGPoint(x: cityBack.position.x - self.backgroundVelocity2, y: cityBack.position.y)
         cityFront2.position = CGPoint(x: cityFront2.position.x - self.backgroundVelocity1, y: cityFront2.position.y)
         cityBack2.position = CGPoint(x: cityBack2.position.x - self.backgroundVelocity2, y: cityBack2.position.y)
-        
+        cityFront3.position = CGPoint(x: cityFront3.position.x - self.backgroundVelocity1, y: cityFront3.position.y)
+        cityBack3.position = CGPoint(x: cityBack3.position.x - self.backgroundVelocity2, y: cityBack3.position.y)
+                
         // Update sky positions
         sky.position = CGPoint(x: sky.position.x - self.skyVelocity, y: sky.position.y)
         sky2.position = CGPoint(x: sky2.position.x - self.skyVelocity, y: sky2.position.y)
@@ -82,7 +107,7 @@ class Runner: SKScene{
         
         // Reset positions for continuous scrolling
         if cityFront.position.x <= -cityFront.size.width {
-                cityFront.position.x = cityFront2.position.x + cityFront2.size.width
+            cityFront.position.x = cityFront2.position.x + cityFront2.size.width
         }
         if cityFront2.position.x <= -cityFront2.size.width {
             cityFront2.position.x = cityFront.position.x + cityFront.size.width
@@ -92,6 +117,12 @@ class Runner: SKScene{
         }
         if cityBack2.position.x <= -cityBack2.size.width {
             cityBack2.position.x = cityBack.position.x + cityBack.size.width
+        }
+        if cityFront3.position.x <= -cityFront3.size.width {
+            cityFront3.position.x = cityBack2.position.x + cityBack2.size.width
+        }
+        if cityBack3.position.x <= -cityBack3.size.width {
+            cityBack3.position.x = cityFront3.position.x + cityFront3.size.width
         }
         
         // Reset sky positions for continuous scrolling
@@ -105,7 +136,12 @@ class Runner: SKScene{
             sky3.position.x = sky2.position.x + sky2.size.width
         }
     }
-
+    func didBegin(_ contact: SKPhysicsContact) {
+        if (contact.bodyA.categoryBitMask == 1 && contact.bodyB.categoryBitMask == 2) ||
+            (contact.bodyA.categoryBitMask == 2 && contact.bodyB.categoryBitMask == 1) {
+            print("Collision detected!")
+        }
+    }
 
 
     /*
