3954acb16f4f4e176939d6fd56095089f480b57d
tess <tessritter15@gmail.com>
2023-11-10 16:11:38 -0500

    added levels

--- RetroGame/MainScreen.swift
+++ RetroGame/MainScreen.swift
@@ -25,19 +25,53 @@ class MainScreen: SKScene {
         menuBar = SKSpriteNode(imageNamed: "SideMenuClosed")
         runnerButton = SKSpriteNode(imageNamed: "RunnerButton")
         
+        //levels
+        let hunger = SKSpriteNode(imageNamed: "100Hunger")
+        let social = SKSpriteNode(imageNamed: "100social")
+        let hygiene = SKSpriteNode(imageNamed: "100Hygiene")
+        let energy = SKSpriteNode(imageNamed: "100Energy")
+        let happy = SKSpriteNode(imageNamed: "100Happy")
+        
+        
         runnerButton?.position = CGPoint(x: size.width * 0.8, y: size.height * 0.7)
         
         menuBar?.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+        
+        //level position
+        hunger.position = CGPoint(x: 100, y: 350)
+        happy.position = CGPoint(x: 100, y: 310)
+        hygiene.position = CGPoint(x: 100, y: 270)
+        energy.position = CGPoint(x: 250, y: 350)
+        social.position = CGPoint(x: 250, y: 310)
+        
+        hunger.zPosition = 1
+        social.zPosition = 1
+        hygiene.zPosition = 1
+        energy.zPosition = 1
+        happy.zPosition = 1
         
         backgroundColor = SKColor.white
         room.setScale(0.85)
         runnerButton?.setScale(0.21)
         menuBar?.setScale(3)
-
+        
+        //level scale
+        hunger.setScale(2)
+        social.setScale(2)
+        hygiene.setScale(2)
+        energy.setScale(2)
+        happy.setScale(2)
+        
         
         room.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
         player.position = CGPoint(x: size.width * 0.45, y: size.height * 0.4)
 
+        
+        addChild(hunger)
+        addChild(social)
+        addChild(energy)
+        addChild(hygiene)
+        addChild(happy)
         addChild(room)
         addChild(player)
         addChild(runnerButton!)
