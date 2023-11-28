839ac7f474abb649754e60a68fb91d02af4a4dbd
freyja feeney <freyjafeeney@freyjas-MacBook-Pro.local>
2023-11-27 23:12:26 -0500

    Added Firebase Authentication/User Creation

---
+++ RetroGame/User/AuthScene.swift
@@ -0,0 +1,14 @@
+//
+//  AuthScene.swift
+//  RetroGame
+//
+//  Created by freyja feeney on 11/26/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct AuthScene: View {
+    @State private var user = ""
+    @State private var pin = ""
+}
---
+++ RetroGame/User/MainScreenWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  MainScreenWrapper.swift
+//  RetroGame
+//
+//  Created by freyja feeney on 11/27/23.
+//
+
+import Foundation
