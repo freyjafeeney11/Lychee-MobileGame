8757a480389aa4b09c1b2e96a2a73088f3ae0b0f
freyja feeney <freyjafeeney@freyjas-MacBook-Pro.local>
2023-12-01 14:48:43 -0500

    bathscene

--- RetroGame/BathScene.swift
+++ RetroGame/BathScene.swift
@@ -1,5 +1,5 @@
 //
-//  MainScreen.swift
+//  BathScene.swift
 //  testing game
 //
 //  Created by freyja feeney on 10/19/23.
@@ -12,9 +12,9 @@ import Firebase
 import _SpriteKit_SwiftUI
 
 class BathScene: SKScene {
+    private var currentNode: SKNode?
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-    var runnerButton: SKSpriteNode?
     var menuBar: SKSpriteNode?
     var edit = EditUser()
     
@@ -33,122 +33,52 @@ class BathScene: SKScene {
         let room = SKSpriteNode(imageNamed: "backgroundTiles")
         let tub = SKSpriteNode(imageNamed: "bathtub")
         let player = SKSpriteNode(imageNamed: "catbat_prototype")
-        
-        runnerButton = SKSpriteNode(imageNamed: "RunnerButton")
-        
-//        func addRandom() -> Double {
-//            let randomDouble = Double.random(in: 0.9...2)
-//            return randomDouble
-//        }
-        //walk animation
-        // setting movement
-        let moveDistance: CGFloat = 150.0
-        let moveDuration: TimeInterval = 0.8
-        
-        // stay still for 5
-        let wait = SKAction.wait(forDuration: 5)
-        
-        let moveLeft = SKAction.moveBy(x: -moveDistance, y: 0, duration: moveDuration)
-        let moveRight = SKAction.moveBy(x: moveDistance, y: 0, duration: moveDuration)
-        
+        let bubbles = SKSpriteNode(imageNamed: "Bubbles")
+        let shampoo = SKSpriteNode(imageNamed: "Shampoo")
+        let sponge = SKSpriteNode(imageNamed: "Sponge")
+        sponge.name = "draggable"
+
         //setting animation
-        let tex1 = SKTexture(imageNamed: "batcat_run1")
-        let tex2 = SKTexture(imageNamed: "batcat_run2")
-        let tex3 = SKTexture(imageNamed: "batcat_run3")
-        let tex4 = SKTexture(imageNamed: "batcat_run4")
-        let walking = [tex1, tex2, tex3, tex4]
-        
-        // change sprite to sitting when sitting
-        let sittingSprite = SKTexture(imageNamed: "catbat_ver2-export.png")
-        let sitAction = SKAction.setTexture(sittingSprite)
-        
-        // might add in an idle animation, or smth like paw licking for the cat and feathers for the chicken
-        // eventually will set this to random distance + time within a limit for less repetitive movement
-        
-        // walking animation
-        let walkingAnimation = SKAction.animate(with: walking, timePerFrame: 0.13)
-        // this action plays the walking animation
-        
-        // try doing distance based off of count of animations, reverse
-        
-        let walkAction = SKAction.repeat(walkingAnimation, count: Int(floor(2.0)))
-        
-        // move left and walk
-        let moveAndAnimateLeft = SKAction.group([moveLeft, walkAction])
-        // move right and walk
-        let moveAndAnimateRight = SKAction.group([moveRight, walkAction])
+        let tex1 = SKTexture(imageNamed: "catbat_idle1")
+        let tex2 = SKTexture(imageNamed: "catbat_idle2")
+        let tex3 = SKTexture(imageNamed: "catbat_idle3")
+        let bathing = [tex1, tex2, tex3]
 
-        // flipping left and right
-        let flipLeft = SKAction.scaleX(to: 1, duration: 0.0)
-        let flipRight = SKAction.scaleX(to: -1, duration: 0.0)
-
-        // sequence where flip left for move left
-        let moveLeftAndFlip = SKAction.sequence([flipLeft, moveAndAnimateLeft, sitAction, wait])
-        // flip right for move right
-        let moveRightAndFlip = SKAction.sequence([flipRight, moveAndAnimateRight, sitAction, wait])
-
-        // repeat the movement forever
-        let moveAndAnimateRepeat = SKAction.repeatForever(SKAction.sequence([moveLeftAndFlip, moveRightAndFlip]))
-
-        // initial scale to face right
-        player.xScale = 1
-
+        let bathingIdle = SKAction.animate(with: bathing, timePerFrame: 0.3)
+        
+        let walkAction = SKAction.repeatForever(bathingIdle)
         // entire sequence forever
-        player.run(moveAndAnimateRepeat)
+        player.run(walkAction)
         
-        //levels
-        let hunger = SKSpriteNode(imageNamed: "100Hunger")
-        let social = SKSpriteNode(imageNamed: "100social")
-        let hygiene = SKSpriteNode(imageNamed: "100Hygiene")
-        let energy = SKSpriteNode(imageNamed: "100Energy")
-        let happy = SKSpriteNode(imageNamed: "100Happy")
-        
-        runnerButton?.position = CGPoint(x: size.width * 0.8, y: size.height * 0.7)
-        
-        //level position
-        // commented these out for levels menu instead
-//        hunger.position = CGPoint(x: 100, y: 350)
-//        happy.position = CGPoint(x: 100, y: 310)
-//        hygiene.position = CGPoint(x: 100, y: 270)
-//        energy.position = CGPoint(x: 250, y: 350)
-//        social.position = CGPoint(x: 250, y: 310)
-//
-//        hunger.zPosition = 1
-//        social.zPosition = 1
-//        hygiene.zPosition = 1
-//        energy.zPosition = 1
-//        happy.zPosition = 1
+
         
         backgroundColor = SKColor.white
-        room.setScale(3.4)
-        tub.setScale(2.6)
-        runnerButton?.setScale(0.21)
+        room.setScale(0.48)
+        tub.setScale(0.8)
+        bubbles.setScale(0.6)
+        player.setScale(1.25)
         
-        //level scale
-        hunger.setScale(2)
-        social.setScale(2)
-        hygiene.setScale(2)
-        energy.setScale(2)
-        happy.setScale(2)
+        
         
         //menu
         menuBar?.setScale(0.8)
-        menuBar?.position = CGPoint(x: -237, y: size.height * 0.5)
+        menuBar?.position = CGPoint(x: -248, y: size.height * 0.5)
         
-        room.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
-        tub.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
-        player.position = CGPoint(x: size.width * 0.45, y: size.height * 0.4)
+        room.position = CGPoint(x: size.width * 0.5, y: size.height * 0.45)
+        tub.position = CGPoint(x: size.width * 0.55, y: size.height * 0.4)
+        bubbles.position = CGPoint(x: size.width * 0.55, y: size.height * 0.42)
+        shampoo.position = CGPoint(x: size.width * 0.7, y: size.height * 0.3)
+        sponge.position = CGPoint(x: size.width * 0.35, y: size.height * 0.15)
+        player.position = CGPoint(x: size.width * 0.62, y: size.height * 0.43)
         
-        addChild(hunger)
-        addChild(social)
-        addChild(energy)
-        addChild(hygiene)
-        addChild(happy)
         addChild(room)
-        addChild(tub)
         addChild(player)
-        addChild(runnerButton!)
+        addChild(tub)
+        addChild(bubbles)
+        addChild(shampoo)
         addChild(menuBar!)
+        // node is sponge
+        addChild(sponge)
         
     }
 
@@ -156,23 +86,30 @@ class BathScene: SKScene {
         for touch in touches {
             let location = touch.location(in: self)
 
-            if runnerButton?.contains(location) == true {
-                
-                // Transition to the runner game scene
-                let runnerGame = Runner(size: size)
-                runnerGame.scaleMode = .aspectFill
-                view?.presentScene(runnerGame)
-            }
             
             if menuBar?.contains(location) == true {
                 let menu = SideMenu(size: size)
                 menu.scaleMode = .aspectFill
                 view?.presentScene(menu)
             }
+            if let touch = touches.first {
+                let location = touch.location(in: self)
+                
+                let touchedNodes = self.nodes(at: location)
+                for node in touchedNodes.reversed() {
+                    if node.name == "draggable" {
+                        self.currentNode = node
+                    }
+                }
+            }
         }
     }
-    
-
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first, let node = self.currentNode {
+            let touchLocation = touch.location(in: self)
+            node.position = touchLocation
+        }
+    }
 }
 
 struct BathView: View {
