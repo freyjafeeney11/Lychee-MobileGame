3e40cc80ba71cfa55598368661bd9ff2a379bc8f
Therese Ritter <98991018+taritter@users.noreply.github.com>
2023-12-04 23:25:58 -0500

    Update ContentViewChat.swift
    documentation for contentViewChat

--- RetroGame/Chat/ContentViewChat.swift
+++ RetroGame/Chat/ContentViewChat.swift
@@ -11,6 +11,7 @@ import SwiftUI
 /*
 struct ContentViewChat: View{
 
+    //var to see if button was pushed, if it was go back to main screen
     @State private var goMain = false
     @ObservedObject private var messageManager = MessageManager()
     @State private var newMessageText = ""
@@ -18,7 +19,10 @@ struct ContentViewChat: View{
 
     var body: some View{
         VStack{
+            //scroll view to see list
             ScrollView{
+                //prints out each message from messagemanager object
+                //message manager object has array of messages
                 ForEach(messageManager.messages, id: \.id) { message in
                     ChatBubble(message: message)
                 }
@@ -26,20 +30,22 @@ struct ContentViewChat: View{
             .padding(.top, 10)
             .background(.white)
         }
-        .background(Color("Peach"))
+        .background(Color("Blue"))
         
         HStack{
             TextField(placeholder: Text("Type a message"), text: $newMessageText)
                 .padding()
                 .background(Color.gray.opacity(0.2))
                 .cornerRadius(10)
-            
+
+            //send button called send message which adds a new message to array
             Button("Send") {
                 messageManager.sendMessage(text: newMessageText)
                 newMessageText = ""
             }
             
         }
+        //go back to main screen if pushed
         Button("back"){
             goMain = true
         }
